3cf6d064fad28f7c6f78f09bfba0eb7b88248fae
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-04 23:16:13 +0530

    Add error 404 if file not found into response

--- Server-Implementation/staticFileHandler.js
+++ Server-Implementation/staticFileHandler.js
@@ -14,32 +14,53 @@ function servingStaticFiles(headers, client) {
   const path = headers['start-line'].split(' ')[1]
   if(path === '/') {
     fs.readFile('./static-files/index.html', (err, content) => {
-      if(err) console.log(err)
+      if(err) {
+        console.log(err)
+        if(err.errno === -2) {
+          client.write(`HTTP/1.1 404 Not Found
+                        Content-Type : text/html
+                        \r\n
+                        <html><body>Not Found!</body></html>`)
+          client.end()
+        }
+      } else {
 
-      let responseHeaders =
-      `HTTP/1.1 200 OK
-      Content-Type : text/html
-      Connection : keep-alive
-      Content-Length : ${content.toString().length}
-      \r\n
-      `
-      let response = Buffer.concat([Buffer.from(responseHeaders), content])
-      client.write(response)
-      client.end()
+        let responseHeaders =
+        `HTTP/1.1 200 OK
+        Content-Type : text/html
+        Connection : keep-alive
+        Content-Length : ${content.toString().length}
+        \r\n
+        `
+        let response = Buffer.concat([Buffer.from(responseHeaders), content])
+        client.write(response)
+        client.end()
+      }
     })
   } else {
     fs.readFile('./static-files' + path, (err, content) => {
-      if(err) console.log(err)
-      let responseHeaders =
-      `HTTP/1.1 200 OK
-      Content-Type : ${mimeType[path.slice(path.lastIndexOf('.'))]}
-      Connection : keep-alive
-      Content-Length : ${content.toString().length}
-      \r\n
-      `
-      let response = Buffer.concat([Buffer.from(responseHeaders), content])
-      client.write(response)
-      client.end()
+      if(err) {
+        console.log(err)
+        if(err.errno === -2) {
+          client.write(`HTTP/1.1 404 Not Found
+                        Content-Type : text/html
+                        \r\n
+                        <html><body>Not Found!</body></html>`)
+          client.end()
+        }
+      } else {
+
+        let responseHeaders =
+        `HTTP/1.1 200 OK
+        Content-Type : ${mimeType[path.slice(path.lastIndexOf('.'))]}
+        Connection : keep-alive
+        Content-Length : ${content.toString().length}
+        \r\n
+        `
+        let response = Buffer.concat([Buffer.from(responseHeaders), content])
+        client.write(response)
+        client.end()
+      }
     })
   }
 }
