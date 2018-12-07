8cc484a0687eb01e988b8be930addc01cc658deb
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-07 08:39:24 +0530

    Structured

--- Server-Implementation/staticFileHandler.js
+++ Server-Implementation/staticFileHandler.js
@@ -1,59 +1,58 @@
 const fs = require('fs')
 
-const mimeType = {
+const mimeTypes = {
   '.html' : 'text/html',
   '.css' : 'text/css',
-  '.js' : 'text/javascript',
-  '.json' : 'application.json',
+  '.js' : 'application/javascript',
+  '.json' : 'application/json',
+  '.txt' : 'text/plain',
   '.jpg' : 'image/jpeg',
   '.jpeg' : 'image/jpeg',
   '.png' : 'image/png'
 }
 
-function servingStaticFiles(headers, client) {
-  const path = headers['start-line'].split(' ')[1]
+function servingStaticFiles(request, dir, client) {
+  console.log('incoming');
+  const path = request.startLine.path
+
   if(path === '/') {
-    fs.readFile('./static-files/index.html', (err, content) => {
+    fs.readFile('./' + dir + '/index.html', (err, content) => {
       if(err) {
         console.log(err)
-        if(err.errno === -2) {
-          client.write(`HTTP/1.1 404 Not Found
-                        Content-Type : text/html\n\r <html><body>Not Found!</body></html>`)
-          client.end()
-        }
       } else {
 
-        let responseHeaders =
-        `HTTP/1.1 200 OK
-        Content-Type : text/html
-        Connection : keep-alive
-        Content-Length : ${content.toString().length}
-        \n\n`
+let responseHeaders =
+`HTTP/1.1 200 OK
+Content-Type: text/html
+Connection: keep-alive
+Content-Length: ${content.toString().length - 1}
+Date: ${new Date()}
+Cache-Control: public, max-age=0
+\r\n`
+        // console.log(responseHeaders)
         let response = Buffer.concat([Buffer.from(responseHeaders), content])
         client.write(response)
-        client.end()
+        // client.end()
       }
     })
   } else {
-    fs.readFile('./static-files' + path, (err, content) => {
+    fs.readFile('./' + dir + path, (err, content) => {
       if(err) {
         console.log(err)
-        if(err.errno === -2) {
-          client.write(`HTTP/1.1 404 Not Found
-                        Content-Type : text/html\n\r <html><body>Not Found!</body></html>`)
-          client.end()
-        }
       } else {
 
         let responseHeaders =
-        `HTTP/1.1 200 OK
-        Content-Type : ${mimeType[path.slice(path.lastIndexOf('.'))]}
-        Connection : keep-alive
-        Content-Length : ${content.toString().length}
-        \n\n` //Required new lines and termination to avoid extra spaces in the beginning of the body
+`HTTP/1.1 200 OK
+Content-Type: ${mimeTypes[path.slice(path.lastIndexOf('.'))]}
+Connection: keep-alive
+Content-Length: ${content.toString().length - 1}
+Date: ${new Date()}
+Cache-Control: public, max-age=0
+\r\n` //CRLF
+
         let response = Buffer.concat([Buffer.from(responseHeaders), content])
         client.write(response)
-        client.end()
+        // client.end()
       }
     })
   }
