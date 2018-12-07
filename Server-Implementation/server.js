f85eda53dd30e081338a41f0352f68a669b6ec8b
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-07 13:11:20 +0530

    Seperate various processing into seperate files

--- Server-Implementation/server.js
+++ Server-Implementation/server.js
@@ -1,10 +1,10 @@
 const net = require('net')
 
-const staticFileHandler = require('./staticFileHandler')
-const routeHandler = require('./routeHandler')
+const parseRequest = require('./requestPipeline/parseRequest')
+const staticFileHandler = require('./handlers/staticFileHandler')
+const routeHandler = require('./handlers/routeHandler')
 const sendErrorStatusCode = require('./sendErrorStatusCode')
 
-const defaultHandlers = [staticFileHandler, routeHandler, errorHandler]
 
 function createServer(port = 8080) {
   const server = net.createServer()
@@ -25,8 +25,7 @@ function createServer(port = 8080) {
       if(!request)  return //The required msg would have already been given
       else {
       this.request = request
-      staticFileHandler(request, this.staticDir, client, next)
-      // if(!routeHandler(request, this.routes, client))
+      this.staticFileHandler(request, this.staticDir, client)
       }
     })
 
@@ -35,7 +34,7 @@ function createServer(port = 8080) {
     })
 
     client.on('close', () => {
-      console.log('SERVER : The socket connection is closed by client')
+      console.log('SERVER : The socket connection is closed')
     })
 
     client.setTimeout(5000)
@@ -52,95 +51,6 @@ function createServer(port = 8080) {
     console.log(err)
     sendErrorStatusCode('500', client)
   })
-}
-
-
-function parseRequest(data, client) {
-  console.log(data.toString())
-  console.log('In parsing request')
-  var request = {}
-  let headerData = data.toString().slice(0, data.toString().indexOf('\r\n\r\n'))
-  let bodyData = data.toString().slice(data.toString().indexOf('\r\n\r\n') + 4)
-
-  var headers = {}
-
-  headerData = headerData.split('\r\n')
-  let startLine = headerData[0].split(' ')
-  let firstLine = {}
-  if(startLine.length > 3) {
-    sendErrorStatusCode('400', client)
-  }
-  firstLine['method'] = startLine[0]
-  firstLine['url'] = startLine[1]
-  firstLine['httpVersion'] = startLine[2]
-
-  parseUrl(firstLine)
-  request.startLine = firstLine
-
-  headerData = headerData.slice(1)
-  for(let item of headerData) {
-    if(item.includes(':')) {
-      let headerKey = item.slice(0, item.indexOf(':')).toLowerCase()
-      if(headerKey.includes(' ')) {
-        sendErrorStatusCode('400', client) //Header key shouldnt have a space
-      } else headers[headerKey] = item.slice(item.indexOf(':') + 2)
-    }
-  }
-
-  request.headers = headers
-  request.body = bodyData
-  if(validateRequest(request, client)) //Validating the request
-    return request
-  return false
-}
-
-function parseUrl(startLine) {
-  console.log('In parse URL')
-  if(!startLine.url.includes('?')) {
-    startLine.path = startLine.url
-  } else {
-    let path = startLine.url.slice(0, startLine.url.indexOf('?'))
-    let queryStrings = startLine.url.slice(startLine.url.indexOf('?') + 1).split('&')
-    let params = {}
-    for(let query of queryStrings) {
-      params[query.slice(0, query.indexOf('='))] = query.slice(query.indexOf('=') + 1)
-    }
-    startLine.path = path
-    startLine.queryStrings = params
-  }
-}
-
-function validateRequest(request, client) {
-  console.log('In validating request')
-  if(!request.startLine.httpVersion.includes('HTTP/1.1')) {
-    sendErrorStatusCode('505', client)
-    return false
-  }
-  else if(request.startLine.method !== 'GET' && request.startLine.method !== 'POST') {
-    sendErrorStatusCode('501', client)
-    return false
-  }
-  else if(request.startLine.method === 'POST' && request.headers['content-length'] === undefined) {
-    sendErrorStatusCode('400', client)
-    return false
-  }
-  else if(request.headers['content-length'] !== undefined && (Number(request.headers['content-length']) !== request.body.length)) {
-    sendErrorStatusCode('400', client)
-    return false
-  }
-  else if(request.body.length > 0 && request.headers['content-length'] === undefined) {
-    sendErrorStatusCode('411', client)
-    return false
-  }
-  else if(request.headers['content-length'] !== undefined && (Number(request.headers['content-length']) !== request.body.length)) {
-    sendErrorStatusCode('400', client)
-    return false
-  }
-  else if(request.startLine.url.length > 100) {
-    sendErrorStatusCode('411', client)
-    return false
-  }
-  return true
 }
 
 function staticServe(directory) {
@@ -161,26 +71,25 @@ function errorHandler(client) {
   sendErrorStatusCode('404', client)
 }
 
-function next() {
-  currentHandler = 0
-  defaultHandlers[++currentHandler]()
-}
 
-var myServer = {
-  createServer : createServer,
+const myServer = {
+  createServer,
   staticDir : 'public',
-  staticServe : staticServe,
-  addRoute : addRoute,
+  staticServe,
+  addRoute,
   routes : {
     GET : {},
     POST : {}
-  }
+  },
+  staticFileHandler,
+  routeHandler,
+  errorHandler
 }
 
 
 myServer.createServer(5000)
-// myServer.staticServe('customPublic')
-myServer.addRoute('GET', '/', () => {
+
+myServer.addRoute('GET', '/echo', () => {
   console.log('i am god')
 })
 myServer.addRoute('POst', '/', () => {
