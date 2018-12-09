298d348d00675905bd3697062ce17deaffda0abf
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-09 18:45:02 +0530

    Remove switch statements to simplify

--- Server-Implementation/handlers/routeHandler.js
+++ Server-Implementation/handlers/routeHandler.js
@@ -1,5 +1,3 @@
-const fs = require('fs')
-
 const mimeTypes = {
   '.html' : 'text/html',
   '.css' : 'text/css',
@@ -14,27 +12,12 @@ const mimeTypes = {
 function routeHandler(request, routes, client) {
   console.log('Route Handler')
   const path = request.startLine.path
-  let queryStrings
-  if(request.startLine.queryStrings) queryStrings = request.startLine.queryStrings
-  console.log(path)
 
-  const getRoutes = routes.GET
-  const postRoutes = routes.POST
-
-  switch(request.startLine.method) {
-    case 'GET' :  if(getRoutes.hasOwnProperty(path)) {
-                    console.log(`GET at ${path}`)
-                    getRoutes[path]()
-                  }
-                  else this.errorHandler(client)
-                  break
-    case 'POST' : if(getRoutes.hasOwnProperty(path)) {
-                    console.log(`POST at ${path}`)
-                    getRoutes[path]()
-                  }
-                  else this.errorHandler(client)
-                  break
-  }
+  if(routes[`${request.startLine.method}`].hasOwnProperty(path)) {
+    console.log(`${request.startLine.method} at ${path}`)
+    routes[`${request.startLine.method}`][path](request, {})
+  } else this.errorHandler(client)
+  return
 }
 
 module.exports = routeHandler
