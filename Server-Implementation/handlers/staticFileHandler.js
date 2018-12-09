88d2d9181ffdf92cf030522467fc8cab96d2d436
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-09 18:43:39 +0530

    Remove switch statements to simplify

--- Server-Implementation/handlers/staticFileHandler.js
+++ Server-Implementation/handlers/staticFileHandler.js
@@ -13,47 +13,27 @@ const mimeTypes = {
 
 function servingStaticFiles(request, dir, client, next) {
   console.log('Static file handler')
-  const path = request.startLine.path
-  if(path === '/') {
-    fs.readFile('./' + dir + '/index.html', (err, content) => {
-      if(err) {
-        console.log(err)
-        this.routeHandler(request, this.routes, client)
-      } else {
-
-let responseHeaders =
-`HTTP/1.1 200 OK
-Content-Type: text/html
-Connection: keep-alive
-Content-Length: ${content.toString().length - 1}
-Date: ${new Date()}
-Cache-Control: public, max-age=0
-\r\n`
-        let response = Buffer.concat([Buffer.from(responseHeaders), content])
-        client.write(response)
-      }
-    })
-  } else {
-    fs.readFile('./' + dir + path, (err, content) => {
-      if(err) {
-        console.log(err)
-        this.routeHandler(request, this.routes, client)
-      } else {
-
-        let responseHeaders =
+  let path = request.startLine.path
+  if(path === '/') path = '/index.html'
+  fs.readFile('./' + dir + path, (err, content) => {
+    if(err) {
+      console.log(err)
+      this.routeHandler(request, this.routes, client)
+    } else {
+      let responseHeaders =
 `HTTP/1.1 200 OK
 Content-Type: ${mimeTypes[path.slice(path.lastIndexOf('.'))]}
 Connection: keep-alive
 Content-Length: ${content.toString().length - 1}
 Date: ${new Date()}
-Cache-Control: public, max-age=0
+Cache-Control: public, max-age=31536000
 \r\n` //CRLF
 
-        let response = Buffer.concat([Buffer.from(responseHeaders), content])
-        client.write(response)
-      }
-    })
-  }
+      let response = Buffer.concat([Buffer.from(responseHeaders), content])
+      client.write(response)
+    }
+  })
+  return
 }
 
 module.exports = servingStaticFiles
