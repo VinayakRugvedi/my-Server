77a1d74a462d32d5201e62a5a18985271f7250e2
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-12-09 23:26:15 +0530

    Implement a trivial response send method

--- Server-Implementation/handlers/routeHandler.js
+++ Server-Implementation/handlers/routeHandler.js
@@ -11,13 +11,27 @@ const mimeTypes = {
 
 function routeHandler(request, routes, client) {
   console.log('Route Handler')
+  var response = {
+    send : send
+  }
   const path = request.startLine.path
 
   if(routes[`${request.startLine.method}`].hasOwnProperty(path)) {
     console.log(`${request.startLine.method} at ${path}`)
-    routes[`${request.startLine.method}`][path](request, {})
+    routes[`${request.startLine.method}`][path](request, response)
   } else this.errorHandler(client)
-  return
+
+  function send() {
+    let body = JSON.stringify({Hello : 'World'})
+    let responseHeaders =
+`HTTP/1.1 200 OK
+Content-Type: ${mimeTypes[path.slice(path.lastIndexOf('.'))]}
+Connection: keep-alive
+Content-Length: ${body.length}
+Date: ${new Date().toUTCString()}
+\r\n`
+    client.write(Buffer.from(responseHeaders + body))
+  }
 }
 
 module.exports = routeHandler
